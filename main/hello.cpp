5c468a7d0171d1a8ff0057167ac31aebe19763b6
shreyamp01 <134608809+shreyamp01@users.noreply.github.com>
2025-03-13 16:17:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout<<"CC LAB 8 :PES1UG22CS574\n";
   return 0;
 }
